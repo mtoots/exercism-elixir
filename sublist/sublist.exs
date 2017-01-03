4130b1697490f34b676fa2518c51e5c687f9ade3
Maert Toots <martooc@gmail.com>
2017-01-03 15:19:18 +0900

    Second revision, inspired by other solutions on exercism.io.

--- sublist/sublist.exs
+++ sublist/sublist.exs
@@ -4,50 +4,23 @@ defmodule Sublist do
   and if not whether it is equal or unequal to the second list.
   """
   def compare(a, b) do
-    a_len = length(a)
-    b_len = length(b)
-
-    case {a, b} do
-      {[], []} -> :equal
-      {[nil], []} -> :superlist
-      {[], [nil]} -> :sublist  
-      {a, a} -> :equal
-      {a, b} when a_len < b_len -> 
-        case sublist?(a, b) do
-          true  -> :sublist
-          false -> :unequal
-        end
-      {a, b} when a_len > b_len -> 
-        case sublist?(b, a) do
-          true  -> :superlist
-          false -> :unequal
-        end
-      _ -> :unequal
+    cond do
+      a == b -> :equal
+      _subset?(a, b) -> :sublist
+      _subset?(b, a) -> :superlist
+      true -> :unequal
     end
   end
 
-  def _sublist?([], _) do
-    true
+  defp _subset?([], _), do: true
+  defp _subset?(_, []), do: false
+  defp _subset?(a, b) do
+    _starts_with?(a, b) || _subset?(a, tl(b))
   end
 
-  def _sublist?([h1 | t1], [h2 | t2]) do
-    cond do
-      h1 === h2 -> _sublist?(t1, t2)
-      true -> false
-    end    
-  end
-
-  def sublist?(a, b) do
-    index = Enum.find_index(b, &(&1 === hd(a)))
-
-    cond do
-      is_nil(index) -> false
-      length(b) - index < length(a) -> false
-      true -> 
-        case _sublist?(tl(a), Enum.slice(b, index + 1, length(b) - index)) do
-          true -> true
-          false -> sublist?(a, Enum.slice(b, index + 1, length(b) - index))
-        end
-    end
+  defp _starts_with?([], _), do: true
+  defp _starts_with?(_, []), do: false
+  defp _starts_with?([x|xs], [y|ys]) do
+    if x === y, do: _starts_with?(xs, ys)
   end
 end
